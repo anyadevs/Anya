3038a664bb2cc3da1b8d7c41802074921ad7bbb6
anyadevs <biloownsthis@gmail.com>
2024-12-27 19:01:05 +0000

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,16 @@
+class BaseModel:
+    def __init__(self):
+        self.trained = False
+
+    def train(self, data):
+        raise NotImplementedError("Train method must be implemented by subclasses.")
+
+    def predict(self, input_data):
+        raise NotImplementedError("Predict method must be implemented by subclasses.")
+
+    def save(self, file_path):
+        raise NotImplementedError("Save method must be implemented by subclasses.")
+
+    def load(self, file_path):
+        raise NotImplementedError("Load method must be implemented by subclasses.")
 
